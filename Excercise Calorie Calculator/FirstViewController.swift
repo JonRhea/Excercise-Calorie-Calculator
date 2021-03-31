a3534c4f29a40af7ca9f35d7cfdf93950876feec
Jonathan Rhea <indianarhea@gmail.com>
2021-03-31 10:23:04 -0400

    Completed App - 3/31

--- Excercise Calorie Calculator/FirstViewController.swift
+++ Excercise Calorie Calculator/FirstViewController.swift
@@ -6,10 +6,48 @@
 //
 
 import UIKit
-class FirstViewController: UIViewController{
+
+var userAge: Double?
+var userWeight: Double?
+var userGender: String = "Female"
+
+class FirstViewController: UIViewController, UITextFieldDelegate{
     
     override func viewDidLoad(){
         super.viewDidLoad()
+        self.yourAge.delegate = self
+        self.yourWeight.delegate = self
     }
+    
+    
+    
+    @IBOutlet weak var yourAge: UITextField!
+    @IBOutlet weak var yourWeight: UITextField!
+    @IBOutlet weak var genderButton: UISegmentedControl!
+    
+    @IBAction func genderButton(_ sender: Any) {
+        
+        switch genderButton.selectedSegmentIndex {
+        case 0:
+          userGender = "Female"
+        case 1:
+          userGender = "Male"
+        default:
+           return
+        }//end switch
+    }
+    
+    
+    @IBAction func savePersonalInfo(_ sender: Any) {
+        userWeight = Double(yourWeight.text!)
+        userAge = Double(yourAge.text!)
+    }
+    
+    //method did not seem to matter in the end
+    //func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //self.yourWeight.resignFirstResponder()
+        //self.yourAge.resignFirstResponder()
+        //return true
+    //}
     
 }//end class
--- Excercise Calorie Calculator/SecondViewController.swift
+++ Excercise Calorie Calculator/SecondViewController.swift
@@ -6,10 +6,28 @@
 //
 
 import UIKit
+var userExerciseTime: Double?
+var userHeartRate: Double?
+
+
 class SecondViewController: UIViewController{
     
     override func viewDidLoad(){
         super.viewDidLoad()
+    }
+    
+    @IBOutlet weak var exerciseTime: UITextField!
+    @IBOutlet weak var avgHeartRate: UITextField!
+    
+    @IBAction func saveExerciseInfo(_ sender: Any) {
+        userExerciseTime = Double(exerciseTime.text!)
+        userHeartRate = Double(avgHeartRate.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.exerciseTime.resignFirstResponder()
+        self.avgHeartRate.resignFirstResponder()
+        return true
     }
     
 }//end class
